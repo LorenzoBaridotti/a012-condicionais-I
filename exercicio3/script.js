6ab65315ae91c7789d38b919ce4a112d00017a5c
LorenzoBaridotti <lorenzo.baridotti@hotmail.com>
2022-08-04 20:25:29 -0300

    Exercícios a012

--- exercicio3/script.js
+++ exercicio3/script.js
@@ -1,1 +1,25 @@
-let nacionalidade //= adicione o prompt aqui
+let nacionalidade = prompt('Digite uma nacionalidade')
+
+if(nacionalidade === 'brasileira' ){
+    console.log('brasileira')
+}else if(nacionalidade === 'argentina' ){
+	console.log('argentina')
+}else if(nacionalidade === 'uruguaia' ){
+    console.log('uruguaia')
+}else if(nacionalidade === 'chilena' ){
+    console.log('chilena')
+}else if(nacionalidade === 'colombiana' ){
+    console.log('colombiana')
+}else if(nacionalidade === 'Brasileira' ){
+    console.log('Brasileira')
+}else if(nacionalidade === 'Argentina' ){
+	console.log('Argentina')
+}else if(nacionalidade === 'Uruguaia' ){
+    console.log('Uruguaia')
+}else if(nacionalidade === 'Chilena' ){
+    console.log('Chilena')
+}else if(nacionalidade === 'Colombiana' ){
+    console.log('Colombiana')
+}else{
+    console.log("nacionalidade não encontrada")
+}
